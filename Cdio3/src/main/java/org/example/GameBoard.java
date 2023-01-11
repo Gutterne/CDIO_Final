853d37b679cacfb604326e11240ef4141bdbabd1
Marble <62856659+Marble5@users.noreply.github.com>
2023-01-11 16:10:03 +0100

    BIG HOTFIX

--- Cdio3/src/main/java/org/example/GameBoard.java
+++ Cdio3/src/main/java/org/example/GameBoard.java
@@ -1,14 +1,7 @@
 package org.example;
 
-import gui_fields.GUI_Brewery;
-import gui_fields.GUI_Chance;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Jail;
-import gui_fields.GUI_Refuge;
-import gui_fields.GUI_Shipping;
-import gui_fields.GUI_Start;
-import gui_fields.GUI_Street;
-import gui_fields.GUI_Tax;
+import gui_fields.*;
+
 import java.awt.Color;
 
  public class  GameBoard {
@@ -30,12 +23,14 @@ import java.awt.Color;
 
 //This is the Frontend GUI. Here we display pictures & Make the colors of each field. This is most used for visuals, than the actual backend. Backend, is Board. This use GUI as userinterface.
      public static GUI_Field[] makeFields() {
-         GUI_Field[] board = new GUI_Field[45];
+         GUI_Field[] board = new GUI_Field[41];
+
          int i = 0;
+
          board[i++] = new GUI_Start("Start", "Modtag: 4000", "Modtag 4000kr,-Når man har passeret start", Color.RED, Color.BLACK);
-         board[i++] = new GUI_Street("Rødovre", "Pris:  1200kr", "", "Startleje:  50", new Color(135, 206, 235), Color.BLACK);
+         board[i++] = new GUI_Street("Rødovrevej", "Pris:  1200kr", "", "Startleje:  50", new Color(135, 206, 235), Color.BLACK);
          board[i++] = new GUI_Chance("?", "Prøv lykken", "Ta' et chancekort.", new Color(204, 204, 204), Color.BLACK);
-         board[i++] = new GUI_Street("Hvidovre", "Pris:  1200kr", "Startleje: 50 ", "50", new Color(135, 206, 235), Color.BLACK);
+         board[i++] = new GUI_Street("Hvidovrevej", "Pris:  1200kr", "Startleje: 50 ", "50", new Color(135, 206, 235), Color.BLACK);
          board[i++] = new GUI_Tax( "Skattevæsnet", "Betal 4000kr", "eller 10%", new Color(255,255,255), Color.BLACK);
          board[i++] = new GUI_Shipping("default","Helsingørbåden","Pris 4000kr", "Lineart pr båd" , "500", new Color(105, 156, 250), Color.BLACK);
          board[i++] = new GUI_Street("Roskildevej", "Pris 2000kr", "Startleje : 100", "100", new Color(255, 127, 0), Color.BLACK);
@@ -50,13 +45,12 @@ import java.awt.Color;
          board[i++] = new GUI_Shipping("default","Molslinjen","Pris 4000kr", "Lineart pr båd" , "500", new Color(255, 50, 5), Color.BLACK);
          board[i++] = new GUI_Street("Bernstorffsvej", "Pris:  3600kr", "", "300", new Color(128, 128, 128), Color.BLACK);
          board[i++] = new GUI_Chance("?", "Prøv lykken", "Ta' et chancekort.", new Color(204, 204, 204), Color.BLACK);
-         board[i++] = new GUI_Street("Hellerup", "Pris:  3600kr", "", "300", new Color(128, 128, 128), Color.BLACK);
+         board[i++] = new GUI_Street("Hellerupvej", "Pris:  3600kr", "", "300", new Color(128, 128, 128), Color.BLACK);
          board[i++] = new GUI_Street("Strandvejen", "Pris:  4000kr", "", "350", new Color(128, 128, 128), Color.BLACK);
          board[i++] = new GUI_Refuge("default", "Parkering", "Tag en pause ", "intet sker", new Color(255, 255, 255), Color.BLACK);
-
          board[i++] = new GUI_Street("Trianglen", "Pris:  4400kr", "", "350", new Color(237, 28, 36), Color.BLACK);
-         board[i++] = new GUI_Street("Østerbrogade", "Pris:  4400kr", "", "350", new Color(237, 28, 36), Color.BLACK);
          board[i++] = new GUI_Chance("?", "Prøv lykken", "Ta' et chancekort.", new Color(204, 204, 204), Color.BLACK);
+         board[i++] = new GUI_Street("Østerbrogade", "Pris:  4400kr", "", "350", new Color(237, 28, 36), Color.BLACK);
          board[i++] = new GUI_Street("Grønningen", "Pris : 4800kr", "", "400", new Color(237, 28, 36), Color.BLACK);
          board[i++] = new GUI_Shipping("default","Gedser-båden","Pris 4000kr", "Lineart pr båd" , "500", new Color(105, 156, 250), Color.BLACK);
          board[i++] = new GUI_Street("Bredgade", "Pris : 5200kr", "", "450", new Color(255, 255, 255), Color.BLACK);
