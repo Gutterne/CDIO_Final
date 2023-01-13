0e541bf475730b015cba0cb75e338d6501eaf37b
mathias124 <mfs2001@hotmail.dk>
2023-01-13 12:15:16 +0100

    Fixed Alend & Moiz code. Reverse board corrected values and rent.

--- Cdio3/src/main/java/org/example/ReverseBoard.java
+++ Cdio3/src/main/java/org/example/ReverseBoard.java
@@ -4,51 +4,51 @@ import Fields.*;
 
 public class ReverseBoard {
     Field[] fieldListReverse;
-    /*
+
     public ReverseBoard() {
         fieldListReverse = new Field[41];
         fieldListReverse[0] = new Start("Start", 1, 4000);
-        fieldListReverse[1] = new BuyableField("Rådhuspladsen", "Lilla", 41, 8000);
-        fieldListReverse[2] = new Tax("Tax", 40, 2000);
-        fieldListReverse[3] = new BuyableField("Frederiksberggade", "Purple", 39, 7000);
-        fieldListReverse[4] = new Chance("Prøv Lykken", 38);
-        fieldListReverse[5] = new BuyableField("Rødby-Puttgarden", "Mørkeblå", 37, 4000);
-        fieldListReverse[6] = new BuyableField("Nygade", "Gul", 36, 6400);
-        fieldListReverse[7] = new Chance("Prøv Lykken", 35);
-        fieldListReverse[8] = new BuyableField("Vimmelskaftet", "Gul", 34, 6000);
-        fieldListReverse[9] = new Hardprison("FÆNGSEL!", 33);
-        fieldListReverse[10] = new BuyableField("Amagertorv", "Gul", 32, 6000);
-        fieldListReverse[11] = new Chance("Prøv Lykken", 31);
-        fieldListReverse[12] = new BuyableField("Østergade", "Hvid", 30, 5600);
-        fieldListReverse[13] = new Brewery("Coca Cola", "Rød", 29, 3000,100,200);
-        fieldListReverse[14] = new BuyableField("Kongens Nytorv", "Hvid", 28, 5200);
-        fieldListReverse[15] = new BuyableField("Bredgade", "Hvid", 27, 5200);
-        fieldListReverse[16] = new BuyableField("Gedser - Rostock", "Mørkeblå", 26, 4000);
-        fieldListReverse[17] = new BuyableField("Grønningen", "Rød", 25, 4800);
-        fieldListReverse[18] = new BuyableField("Østerbrogade", "Rød", 24, 4400);
-        fieldListReverse[19] = new Chance("Prøv Lykken", 23);
-        fieldListReverse[20] = new BuyableField("Trianglen", "Rød", 22, 4400);
+        fieldListReverse[1] = new BuyableField("Rådhuspladsen", "Lilla", 41, 8000,1000,4000,12000,28000,34000,40000);
+        fieldListReverse[2] = new Tax("Tax", 2, 2000);
+        fieldListReverse[3] = new BuyableField("Frederiksberggade", "Purple", 3, 7000,700,3500,10000,22000,26000,30000);
+        fieldListReverse[4] = new Metro("Metro","Sort",4);
+        fieldListReverse[5] = new Ferry("Rødby-Puttgarden", "Mørkeblå", 5, 4000,500,1000);
+        fieldListReverse[6] = new BuyableField("Nygade", "Gul", 6, 6400,600,3000,9000,20000,24000,28000);
+        fieldListReverse[7] = new Chance("Prøv Lykken", 7);
+        fieldListReverse[8] = new BuyableField("Vimmelskaftet", "Gul", 8, 6000,550,2600,7800,18000,22000,25000);
+        fieldListReverse[9] = new BuyableField("Amagertorv", "Gul", 9, 6000,550,2600,7800,18000,22000,25000);
+        fieldListReverse[10] = new Hardprison("FÆNGSEL!", 10);
+        fieldListReverse[11] = new Chance("Prøv Lykken", 11);
+        fieldListReverse[12] = new BuyableField("Østergade", "Hvid", 12, 5600, 500,2400,7200,17000,20500,24000);
+        fieldListReverse[13] = new Brewery("Coca Cola", "Rød", 13, 3000,100,200);
+        fieldListReverse[14] = new BuyableField("Kongens Nytorv", "Hvid", 14, 5200, 450,2200,6600,16000,19500,23000);
+        fieldListReverse[15] = new BuyableField("Bredgade", "Hvid", 15, 5200,450,2200,6600,16000,19500,23000);
+        fieldListReverse[16] = new Metro("Metro","black",16);
+        fieldListReverse[17] = new BuyableField("Grønningen", "Rød", 17, 4800,400,2000,6000,15000,18500,22000);
+        fieldListReverse[18] = new BuyableField("Østerbrogade", "Rød", 18, 4400,350,1800,5000,14000,17500,21000);
+        fieldListReverse[19] = new Chance("Prøv Lykken", 19);
+        fieldListReverse[20] = new BuyableField("Trianglen", "Rød", 20, 4400,350,1800,5000,14000,17500,21000);
         fieldListReverse[21] = new Parking("Parkering", "Intet", 21, 0);
-        fieldListReverse[22] = new BuyableField("Strandvejen", "Grøn", 20, 4000);
-        fieldListReverse[23] = new BuyableField("Hellerupvej", "Grå", 19, 3600);
-        fieldListReverse[24] = new Chance("Chance", 18);
-        fieldListReverse[25] = new BuyableField("Bernstorffsvej", "Grå", 17, 3600);
-        fieldListReverse[26] = new BuyableField("Mols-linjen", "Mørkeblå", 16, 4000);
-        fieldListReverse[27] = new BuyableField("Gammel kongevej", "Grøn", 15, 3200);
-        fieldListReverse[28] = new BuyableField("Bülowsvej", "Grøn", 14, 2800);
-        fieldListReverse[29] = new Brewery("Tuborg Squash", "Rød", 13, 3000,100,200);
-        fieldListReverse[30] = new BuyableField("Frederiksberg allé", "Grøn", 12, 2800);
-        fieldListReverse[31] = new Prison("I fængsel (På Besøg)", 11);
-        fieldListReverse[32] = new BuyableField("Allégade", "Orange", 10, 2000);
-        fieldListReverse[33] = new BuyableField("Valby Langgade", "Orange", 9, 2000);
-        fieldListReverse[34] = new Chance("Prøv Lykken", 8);
-        fieldListReverse[35] = new BuyableField("Roskildevej", "Orange", 7, 2000);
-        fieldListReverse[36] = new BuyableField("Helsingør - Helsingborg", "Lyseblå", 6, 4000);
-        fieldListReverse[37] = new Tax("Tax", 5, 4000);
-        fieldListReverse[38] = new BuyableField("Hvidovre", "Lyseblå", 4, 1200);
-        fieldListReverse[39] = new Chance("Prøv Lykken", 3);
-        fieldListReverse[40] = new BuyableField("Rødovre", "Lyseblå", 2, 1200);
+        fieldListReverse[22] = new BuyableField("Strandvejen", "Grøn", 22, 4000,350,1600,4400,12000,16000,20000);
+        fieldListReverse[23] = new BuyableField("Hellerupvej", "Grå", 23, 3600,300,1400,4000,11000,15000,19000);
+        fieldListReverse[24] = new Chance("Chance", 24);
+        fieldListReverse[25] = new BuyableField("Bernstorffsvej", "Grå", 25, 3600,300,1400,4000,11000,15000,19000);
+        fieldListReverse[26] = new Metro("Metro","black",26);
+        fieldListReverse[27] = new BuyableField("Gammel kongevej", "Grøn", 27, 3200,250,1250,3750,10000,14000,18000);
+        fieldListReverse[28] = new BuyableField("Bülowsvej", "Grøn", 28, 2800,200,1000,3000,9000,12500,15000);
+        fieldListReverse[29] = new Brewery("Tuborg Squash", "Rød", 29, 3000,100,200);
+        fieldListReverse[30] = new BuyableField("Frederiksberg allé", "Grøn", 30, 2800,200,1000,3000,9000,12500,15000);
+        fieldListReverse[31] = new Prison("I fængsel (På Besøg)", 31);
+        fieldListReverse[32] = new BuyableField("Allégade", "Orange", 32, 2000,150,800,2000,6000,9000,12000);
+        fieldListReverse[33] = new BuyableField("Valby Langgade", "Orange", 33, 2000,100,600,1800,5400,8000,11000);
+        fieldListReverse[34] = new Metro("Metro","black",34);
+        fieldListReverse[35] = new BuyableField("Roskildevej", "Orange", 35, 2000,100,600,1800,5400,8000,11000);
+        fieldListReverse[36] = new Ferry("Helsingør - Helsingborg", "Lyseblå", 36, 4000,500,1000);
+        fieldListReverse[37] = new Tax("Tax", 37, 4000);
+        fieldListReverse[38] = new BuyableField("Hvidovre", "Lyseblå", 38, 1200,50,250,400,750,2250,6000);
+        fieldListReverse[39] = new Chance("Prøv Lykken", 39);
+        fieldListReverse[40] = new BuyableField("Rødovre", "Lyseblå", 40, 1200,50,250,750,2250,4000,6000);
     }
 
-     */
+
 }
