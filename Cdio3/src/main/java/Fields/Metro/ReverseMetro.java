9fde5de76a30d4a9e78adf6ca3399f16f40c29dc
Marble <62856659+Marble5@users.noreply.github.com>
2023-01-17 22:40:43 +0100

    hotfix reverse metro

--- Cdio3/src/main/java/Fields/Metro/ReverseMetro.java
+++ Cdio3/src/main/java/Fields/Metro/ReverseMetro.java
@@ -6,7 +6,7 @@ import org.example.Player;
 //this class is the reverse algorithm of metro, so it can be used on the reverseboard feature.
 public class ReverseMetro extends Field {
     private int cost;
-
+    private boolean taken=false;
     Player travelplayer;
     private String color;
     //this method is referenced in board.java and inherits much of it's properties from fields, it's parallel combared to Metro.
@@ -16,17 +16,20 @@ public class ReverseMetro extends Field {
     }
 
     public void landOndField(Player travelPlayer) {
-        if(travelPlayer.getPositition()%40==4){
-            travelPlayer.setPositition(15);
-        } //second metro stop to third
-        else if (travelPlayer.getPositition()%40==15){
-            travelPlayer.setPositition(25);
-        } //third metro stop to fourth
-        else if(travelPlayer.getPositition()%40==25){
-            travelPlayer.setPositition(33);
-        } //fourth metro stop to first
-        else if(travelPlayer.getPositition()%40==33){
-            travelPlayer.setPositition(4);
-        }
+        if(!taken){
+            if(travelPlayer.getPositition()%40==4){
+                travelPlayer.setPositition(15);
+            } //second metro stop to third
+            else if (travelPlayer.getPositition()%40==15){
+                travelPlayer.setPositition(25);
+            } //third metro stop to fourth
+            else if(travelPlayer.getPositition()%40==25){
+                travelPlayer.setPositition(33);
+            } //fourth metro stop to first
+            else if(travelPlayer.getPositition()%40==33){
+                travelPlayer.setPositition(4);
+            }
+        }        taken=true;
+
     }
 }
